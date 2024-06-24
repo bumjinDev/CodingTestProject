a116f653420891ce36dc3e30b02da7eaad19274f
bumjinDev <bumjin.dev@gmail.com>
2024-06-24 13:08:00 +0900

    프로그래머스 문제 풀이

--- src/LevelTwo/NumberExpression/Solution.java
+++ src/LevelTwo/NumberExpression/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 	  int res = 0;
 	  
 	  for(int step = 1; step <= n; step++) {
-		  System.out.println();
+		  System.out.println("");
 		  for(int i = step; i <= n; i++) {
 			  
 			 res += i;
